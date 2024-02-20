d2faad505f768058f855756aeb36e4f6b8f2aa4c
Natalia Ivantsova <ivantsovana@gmail.com>
2024-02-20 13:44:36 +0200

    feat: add root and add methods to binarysearchtree class

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,59 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.rootNode = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    return this.rootNode;
+  }
+
+  add(data) {
+    if (!data) {
+      return;
+    }
+
+    const newNode = new Node(data);
+
+
+    if (this.rootNode === null) {
+      this.rootNode = newNode;
+      return this;
+    }
+
+    let current = this.rootNode;
+
+    while (current) {
+      if (current.data === data) {
+        return undefined;
+      }
+
+      if (data < current.data) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+
+        current = current.left;
+      }
+
+      if (data > current.data) {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+
+        current = current.right;
+      }
+    }
+
   }
 
   has(/* data */) {
